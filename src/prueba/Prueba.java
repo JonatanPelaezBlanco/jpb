46ec83802ac45e401edaac7bcff3e4af794d2485
Jonatan <Jonatan@Sr_Jota_PC>
2017-03-19 23:31:13 +0100

    1er commit

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -6,6 +6,7 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.println("Inicio de proyecto");
          System.out.println("Inicio de proyecto");
+         System.out.println("I'm studying");
         
     }
     
